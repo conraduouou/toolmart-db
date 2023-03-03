029b169342954ecfa1a18469f5dd4b3b892756a9
Louise <louiserafaellalu@gmail.com>
2023-03-04 01:26:00 +0800

    always enable swagger

--- Program.cs
+++ Program.cs
@@ -36,11 +36,11 @@ var app = builder.Build();
 app.Configuration.GetConnectionString("");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 // app.UseHttpsRedirection();
 

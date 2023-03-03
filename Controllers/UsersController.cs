9bf1dfc288c0b76139251f3d8f5e46ba642f095d
Louise <louiserafaellalu@gmail.com>
2023-03-04 04:27:09 +0800

    only add email in the url

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -22,7 +22,7 @@ public class UsersController : ControllerBase
         return data;
     }
 
-    [HttpGet("api/[controller]/email/{email}")]
+    [HttpGet("email/{email}")]
     public async Task<ActionResult<User>> GetByEmail(string email)
     {
         var data = await _service.GetByEmailAsync(email);

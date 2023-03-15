13f98607358e70b673dec95cba1c4200c643dc78
Louise <louiserafaellalu@gmail.com>
2023-03-15 14:43:56 +0800

    update bson representation type of review

--- Models/Review.cs
+++ Models/Review.cs
@@ -5,8 +5,10 @@ namespace ToolMart.Models;
 
 public class Review : Model
 {
+    [BsonRepresentation(BsonType.ObjectId)]
     public string UserId { get; set; } = null!;
 
+    [BsonRepresentation(BsonType.ObjectId)]
     public string ItemId { get; set; } = null!;
 
     public string? UserComment { get; set; }
